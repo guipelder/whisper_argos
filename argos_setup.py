39f39ff9f6becc4acb35fc55a9966156fcf12a63
guipelder <mehdi_m_hosseini@yahoo.com>
2023-10-11 20:58:22 +0330

    argos_setup.py update

--- argos_setup.py
+++ argos_setup.py
@@ -1,74 +1,118 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-argostranslate.package.install_from_path('./argos_packages_offline/en_es.argosmodel')
-argostranslate.package.install_from_path('./argos_packages_offline/en_fr.argosmodel')
-argostranslate.package.install_from_path('./argos_packages_offline/translate-en_de-1_5.argosmodel')
-argostranslate.package.install_from_path('./argos_packages_offline/translate-en_fa-1_5.argosmodel')
-argostranslate.package.install_from_path('./argos_packages_offline/translate-en_ja-1_1.argosmodel')
-argostranslate.package.install_from_path('./argos_packages_offline/translate-en_ru-1_7.argosmodel')
-argostranslate.package.install_from_path('./argos_packages_offline/translate-en_zh-1_1.argosmodel')
-from_code = "en"
 import argostranslate.package
 import argostranslate.translate
-#install arabic
-#install chinease
-#install espaniol
-#install  french
-#install german
-#install japenease
-#install russian
-print(translatedText)
-print(translatedText)
-print(translatedText)
-print(translatedText)
-print(translatedText)
-print(translatedText)
-print(translatedText)
-to_code = "de"
-to_code = "es"
+ 
+from_code = "en"
 to_code = "fa"
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install farsi
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/translate-en_fa-1_5.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+    # Translate
+    # '¡سلام دنیا!'
+
 to_code = "fr"
-to_code = "ja"
-to_code = "ru"
+
+
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install french
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/en_fr.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+    
+
+
+to_code = "es"
+
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install espaniol
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/en_es.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+    
+
+
 to_code = "zh"
-# Translate
-# Translate
-# Translate
-# Translate
-# Translate
-# Translate
-# Translate
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-# '¡سلام دنیا!'
+
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install chinease
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/translate-en_zh-1_1.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+    
+
+
+
+#install german
+to_code = "de"
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install german
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/translate-en_de-1_5.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+
+
+#install russian
+to_code = "ru"
+
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install russian
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/translate-en_ru-1_7.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+    
+
+
+#install japenease
+to_code = "ja"
+
+try:
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+except:
+    #install japenease
+    print("installing the offline model")
+    argostranslate.package.install_from_path('./argos_packages_offline/translate-en_ja-1_1.argosmodel')
+    translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    print(translatedText)
+
+
+
 
