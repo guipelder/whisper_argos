befd6560845c34ce447bfbb0586a8500840ac470
guipelder <mehdi_m_hosseini@yahoo.com>
2023-10-12 10:04:15 +0330

    argos_setup.py update

--- argos_setup.py
+++ argos_setup.py
@@ -1,10 +1,38 @@
 import argostranslate.package
 import argostranslate.translate
  
+print("trying to install the models online")
+from_code = "en"
+#to_code = "es"
+to_code_list = ["fa","fr","es","zh","de","ru","ja"]
+
+# Download and install Argos Translate package online
+argostranslate.package.update_package_index()
+available_packages = argostranslate.package.get_available_packages()
+for to_code in to_code_list:
+    try: 
+        # Translate
+        translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+        print(translatedText)
+    except:
+
+        package_to_install = next(
+            filter(
+                lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+            )
+        )
+        print("downloading the package")
+        argostranslate.package.install_from_path(package_to_install.download())
+
+
+
+print("trying to install the models offline")
+
 from_code = "en"
 to_code = "fa"
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -13,6 +41,7 @@ except:
     argostranslate.package.install_from_path('./argos_packages_offline/translate-en_fa-1_5.argosmodel')
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
+    
 
     # Translate
     # '¡سلام دنیا!'
@@ -20,6 +49,7 @@ except:
 to_code = "fr"
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -33,6 +63,7 @@ except:
 to_code = "es"
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -45,6 +76,7 @@ except:
 to_code = "zh"
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -59,6 +91,7 @@ except:
 to_code = "de"
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -74,6 +107,7 @@ to_code = "ru"
 
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -88,6 +122,7 @@ except:
 to_code = "ja"
 
 try:
+    print("check if translate model is installed")
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 except:
@@ -97,4 +132,9 @@ except:
     translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
     print(translatedText)
 
+ 
 
+#import argostranslate.package
+#import argostranslate.translate
+
+print("Done installing")
